4364beb7357747b71b0c80e73ceb013940e9de69
Twaza-j <atwazagye@gmail.com>
2023-09-19 21:24:19 +0300

    our_printf

--- _printf.c
+++ _printf.c
@@ -1,80 +1,39 @@
-#include <main.h>
-
-#define BUFFER_SIZE 1024
-
-void outputBuffer(char buffer[], int *buffIndex);
+#include "main.h"
 
 /**
- * _printf - Custom printf function
- * @format: Format string.
- * Return: Number of characters printed.
+ * _printf - display output to stdout.
+ * @format_str: character string.
+ * 
+ * Return: number of characters printed
+ * 
  */
-int _printf(const char *format, ...);
+
+int _printf(const char *format_str, ...)
 {
-    int i, totalPrinted = 0, printedChars = 0;
-    int flags = 0, width = 0, precision = 0, size = 0, buffIndex = 0;
-    va_list args;
-    char outputBuffer[BUFFER_SIZE];
+    size_t char_count = 0;
+    va_list args_list;
 
-    if (format == NULL)
-        return (-1);
-
-    va_start(args, format);
-
-    for (i = 0; format[i] != '\0'; i++)
+    if (format_str != NULL)
     {
-        if (format[i] != '%')
+        va_start(args_list, format_str);
+
+        for (; *format_str; format_str++)
         {
-            outputBuffer[buffIndex++] = format[i];
-            if (buffIndex == BUFFER_SIZE)
-                outputBuffer(outputBuffer, &buffIndex);
-            printedChars++;
-        }
-        else
-        {
-            outputBuffer(outputBuffer, &buffIndex);
-            
-            flags = getFlags(format, &i);
-            width = getWidth(format, &i, args);
-            precision = getPrecision(format, &i, args);
-            size = getSize(format, &i);
-            ++i;
-            totalPrinted = handlePrint(format, &i, args, outputBuffer, flags, width, precision, size);
-            if (totalPrinted == -1)
+            if (*format_str != '%')
+                char_count += write(1, format_str, 1);
+            else if (*format_str == '%' && *(format_str - 1) == 92)
+                char_count += _putchar('%');
+            else
             {
-                va_end(args);
-                return (-1);
+                format_str++;
+                char_count += process_specifiers(format_str, args_list);
             }
-            printedChars += totalPrinted;
         }
+        
+        va_end(args_list);
+        return char_count;
     }
-
-    outputBuffer(outputBuffer, &buffIndex);
-
-    va_end(args);
-
-    return (printedChars);
-}
-
-/**
- * outputBuffer - Prints the contents of the output buffer if it exists
- * @buffer: Array of characters.
- * @buffIndex: Index at which to add the next character, represents the length.
- */
-void outputBuffer(char buffer[], int *buffIndex)
-{
-    if (*buffIndex > 0)
-    {
-        ssize_t bytes_written = write(1, &buffer[0], *buffIndex);
-        if (bytes_written == -1)
-        {
-            perror("write"); 
-            *buffIndex = 0;  
-        }
-        else
-        {
-            *buffIndex = 0;  
-        }
-    }
+    else
+        return (0);
 }
 
