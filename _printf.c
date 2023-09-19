4e13f054b80ce966307c67028eba3140dbc31913
Twaza-j <atwazagye@gmail.com>
2023-09-19 17:13:12 +0300

    our_printf

--- _printf.c
+++ _printf.c
@@ -1,6 +1,6 @@
-#include "main.h"
-#include <stdarg.h>
-#include <stdio.h>
+#include <main.h>
+
+#define BUFFER_SIZE 1024
 
 void outputBuffer(char buffer[], int *buffIndex);
 
@@ -11,46 +11,49 @@ void outputBuffer(char buffer[], int *buffIndex);
  */
 int _printf(const char *format, ...)
 {
-	int i, totalPrinted = 0, printedChars = 0;
-	int flags, width, precision, size, buffIndex = 0;
-	va_list args;
-	char outputBuffer[BUFFER_SIZE];
+    int i, totalPrinted = 0, printedChars = 0;
+    int flags = 0, width = 0, precision = 0, size = 0, buffIndex = 0;
+    va_list args;
+    char outputBuffer[BUFFER_SIZE];
 
-	if (format == NULL)
-		return (-1);
+    if (format == NULL)
+        return (-1);
 
-	va_start(args, format);
+    va_start(args, format);
 
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			outputBuffer[buffIndex++] = format[i];
-			if (buffIndex == BUFFER_SIZE)
-				outputBuffer(outputBuffer, &buffIndex);
-			printedChars++;
-		}
-		else
-		{
-			outputBuffer(outputBuffer, &buffIndex);
-			flags = getFlags(format, &i);
-			width = getWidth(format, &i, args);
-			precision = getPrecision(format, &i, args);
-			size = getSize(format, &i);
-			++i;
-			totalPrinted = handlePrint(format, &i, args, outputBuffer,
-				flags, width, precision, size);
-			if (totalPrinted == -1)
-				return (-1);
-			printedChars += totalPrinted;
-		}
-	}
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] != '%')
+        {
+            outputBuffer[buffIndex++] = format[i];
+            if (buffIndex == BUFFER_SIZE)
+                outputBuffer(outputBuffer, &buffIndex);
+            printedChars++;
+        }
+        else
+        {
+            outputBuffer(outputBuffer, &buffIndex);
+            
+            flags = getFlags(format, &i);
+            width = getWidth(format, &i, args);
+            precision = getPrecision(format, &i, args);
+            size = getSize(format, &i);
+            ++i;
+            totalPrinted = handlePrint(format, &i, args, outputBuffer, flags, width, precision, size);
+            if (totalPrinted == -1)
+            {
+                va_end(args);
+                return (-1);
+            }
+            printedChars += totalPrinted;
+        }
+    }
 
-	outputBuffer(outputBuffer, &buffIndex);
+    outputBuffer(outputBuffer, &buffIndex);
 
-	va_end(args);
+    va_end(args);
 
-	return (printedChars);
+    return (printedChars);
 }
 
 /**
@@ -60,9 +63,18 @@ int _printf(const char *format, ...)
  */
 void outputBuffer(char buffer[], int *buffIndex)
 {
-	if (*buffIndex > 0)
-		write(1, &buffer[0], *buffIndex);
-
-	*buffIndex = 0;
+    if (*buffIndex > 0)
+    {
+        ssize_t bytes_written = write(1, &buffer[0], *buffIndex);
+        if (bytes_written == -1)
+        {
+            perror("write"); 
+            *buffIndex = 0;  
+        }
+        else
+        {
+            *buffIndex = 0;  
+        }
+    }
 }
 
