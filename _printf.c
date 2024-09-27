0559e5d200ee0a9e214170915fa5e7e0a8d2bc7e
Fenet Damena <fenetdamena74@gmail.com>
2024-09-27 06:48:58 -1200

    print

--- _printf.c
+++ _printf.c
@@ -1,67 +1,45 @@
 #include "main.h"
-
-void fa_print_buffer(char buffer[], int *buff_ind);
-
 /**
- * _printf - This is a Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
  */
-int _printf(const char *format, ...)
+int _printf(const char * const format, ...)
 {
-    int i, printed = 0, printed_chars = 0;
-    int flags, width, precision, size, buff_ind = 0;
-    va_list list;
-    char buffer[FA_BUFF_SIZE];
+	convert_match m[] = {
+		{"%s", printf_string}, {"%c", printf_char},
+		{"%%", printf_37},
+		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
+		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
+		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
+		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
+	};
 
-    if (format == NULL)
-        return (-1);
+	va_list args;
+	int i = 0, j, len = 0;
 
-    va_start(list, format);
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
 
-    for (i = 0; format && format[i] != '\0'; i++)
-    {
-        if (format[i] != '%')
-        {
-            buffer[buff_ind++] = format[i];
-            if (buff_ind == FA_BUFF_SIZE)
-                fa_print_buffer(buffer, &buff_ind);
-            /* write(1, &format[i], 1);*/
-            printed_chars++;
-        }
-        else
-        {
-            fa_print_buffer(buffer, &buff_ind);
-            flags = fa_get_flags(format, &i);
-            width = fa_get_width(format, &i, list);
-            precision = fa_get_precision(format, &i, list);
-            size = fa_get_size(format, &i);
-            ++i;
-            printed = fa_process_print(format, &i, list, buffer,
-                flags, width, precision, size);
-            if (printed == -1)
-                return (-1);
-            printed_chars += printed;
-        }
-    }
-
-    fa_print_buffer(buffer, &buff_ind);
-
-    va_end(list);
-
-    return (printed_chars);
+Here:
+	while (format[i] != '\0')
+	{
+		j = 13;
+		while (j >= 0)
+		{
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			{
+				len += m[j].f(args);
+				i = i + 2;
+				goto Here;
+			}
+			j--;
+		}
+		_putchar(format[i]);
+		len++;
+		i++;
+	}
+	va_end(args);
+	return (len);
 }
-
-/**
- * fa_print_buffer - Displays  the contents of the buffer if it exists
- * @buffer: Array of chars
- * @buff_ind: Index to add the next char, depicts the length.
- */
-void fa_print_buffer(char buffer[], int *buff_ind)
-{
-    if (*buff_ind > 0)
-        write(1, &buffer[0], *buff_ind);
-
-    *buff_ind = 0;
-}
-
--- main.h
+++ main.h
@@ -1,107 +1,49 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
 #include <unistd.h>
 
-/**
- * @ FA: Fenet and Agaba
- */
 
-#define FA_UNUSED(x) (void)(x)
-#define FA_BUFF_SIZE 1024
-
-#define FA_MINUS 1
-#define FA_PLUS 2
-#define FA_ZERO 4
-#define FA_HASH 8
-#define FA_SPACE 16
-
-#define FA_LONG 2
-#define FA_SHORT 1
 
 /**
- * struct fa_fmt - Format specification structure
+ * struct format - match the conversion specifiers for printf
+ * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
+ * @f: type pointer to function for the conversion specifier
  *
- * @fa_fmt: format.
- * @fa_fn: function associated.
  */
-struct fa_fmt
+
+typedef struct format
 {
-    char fa_fmt;
-    int (*fa_fn)(va_list, char[], int, int, int, int);
-};
+	char *id;
+	int (*f)();
+} convert_match;
 
-/**
- * typedef struct fa_fmt fa_fmt_t - Format specification structure
- *
- * @fa_fmt: format.
- * @fa_fn: function associated.
- */
-typedef struct fa_fmt fa_fmt_t;
-
+int printf_pointer(va_list val);
+int printf_hex_aux(unsigned long int num);
+int printf_HEX_aux(unsigned int num);
+int printf_exclusive_string(va_list val);
+int printf_HEX(va_list val);
+int printf_hex(va_list val);
+int printf_oct(va_list val);
+int printf_unsigned(va_list args);
+int printf_bin(va_list val);
+int printf_srev(va_list args);
+int printf_rot13(va_list args);
+int printf_int(va_list args);
+int printf_dec(va_list args);
+int _strlen(char *s);
+int *_strcpy(char *dest, char *src);
+int _strlenc(const char *s);
+int rev_string(char *s);
+int _strlenc(const char *s);
+int printf_37(void);
+int printf_char(va_list val);
+int printf_string(va_list val);
+int _putchar(char c);
 int _printf(const char *format, ...);
-int fa_process_print(const char *fa_fmt, int *i,
-                     va_list list, char buffer[], int flags, int width, int precision, int size);
-int fa_print_char(va_list types, char buffer[],
-                  int flags, int width, int precision, int size);
-int fa_print_string(va_list types, char buffer[],
-                    int flags, int width, int precision, int size);
-int fa_print_percent(va_list types, char buffer[],
-                     int flags, int width, int precision, int size);
-
-int fa_print_int(va_list types, char buffer[],
-                 int flags, int width, int precision, int size);
-int fa_print_binary(va_list types, char buffer[],
-                    int flags, int width, int precision, int size);
-int fa_print_unsigned(va_list types, char buffer[],
-                      int flags, int width, int precision, int size);
-int fa_print_octal(va_list types, char buffer[],
-                   int flags, int width, int precision, int size);
-int fa_print_hexadecimal(va_list types, char buffer[],
-                         int flags, int width, int precision, int size);
-int fa_print_hexa_upper(va_list types, char buffer[],
-                        int flags, int width, int precision, int size);
-
-int fa_print_hexa(va_list types, char map_to[],
-                  char buffer[], int flags, char flag_ch, int width, int precision, int size);
-
-int fa_print_non_printable(va_list types, char buffer[],
-                           int flags, int width, int precision, int size);
-
-int fa_print_pointer(va_list types, char buffer[],
-                     int flags, int width, int precision, int size);
-
-int fa_get_flags(const char *format, int *i);
-int fa_get_width(const char *format, int *i, va_list list);
-int fa_get_precision(const char *format, int *i, va_list list);
-int fa_get_size(const char *format, int *i);
-
-int fa_print_reverse(va_list types, char buffer[],
-                     int flags, int width, int precision, int size);
-
-int fa_print_rot13string(va_list types, char buffer[],
-                         int flags, int width, int precision, int size);
-
-int fa_process_write_char(char c, char buffer[], int flags, int width, int precision, int size);
-int fa_write_number(int is_positive, int ind, char buffer[],
-                    int flags, int width, int precision, int size);
-int fa_write_num(int ind, char bff[], int flags, int width, int precision,
-                 int length, char padd, char extra_c);
-int fa_write_pointer(char buffer[], int ind, int length,
-                     int width, int flags, char padd, char extra_c, int padd_start);
-
-int fa_write_unsigned(int is_negative, int ind,
-                      char buffer[],
-                      int flags, int width, int precision, int size);
-
-int fa_is_printable(char);
-int fa_append_hexa_code(char, char[], int);
-int fa_is_digit(char);
-
-long int fa_convert_size_number(long int num, int size);
-long int fa_convert_size_unsigned(unsigned long int num, int size);
 
 #endif
-
